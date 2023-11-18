c4177b980c7998447dbf830812d4e9d62db325ee
12amsag <147137156+12amsag@users.noreply.github.com>
2023-11-18 19:18:05 +0530

    Add files via upload

---
+++ variable.js.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+
+let firstName = "Amsath";
+
+let secondName ="PK"
+
+console.log(""+"");
+console.log(""+"");
+
+
+let number = 1;
+
+console.log(100+number);
+
+let websitename = "ijash"
+console.log
